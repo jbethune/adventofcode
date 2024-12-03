283a584a66064c2e2eaf5af48e58669c69583fe9
Jörn <jbethune@localhost>
2024-12-03 18:39:45 +0100

    multiply directly

--- advent2024/src/day3.rs
+++ advent2024/src/day3.rs
@@ -1,21 +1,16 @@
 use camino::Utf8Path as Path;
 
-struct Mul {
-    a: usize,
-    b: usize,
-}
-
-/// parse factors for a multiplication instruction (if it is valid)
-fn parse_factors(mul_str: &str) -> Option<Mul> {
+/// parse factors and multiply them if the instruction is valid
+fn parse_factors(mul_str: &str) -> Option<usize> {
     let (left, right) = mul_str.split_once(",")?;
     let a: usize = left.parse().ok()?;
     let left = right.split_once(")")?.0;
-    let b = left.parse().ok()?;
-    Some(Mul { a, b })
+    let b: usize = left.parse().ok()?;
+    Some(a * b)
 }
 
-/// gather all active multiplication instructions
-fn gather_muls(parts: &[&str], out: &mut Vec<Mul>) {
+/// gather and execute all active multiplication instructions
+fn gather_muls(parts: &[&str], out: &mut Vec<usize>) {
     for part in parts {
         if let Some(factors) = parse_factors(part) {
             out.push(factors)
@@ -23,7 +18,7 @@ fn gather_muls(parts: &[&str], out: &mut Vec<Mul>) {
     }
 }
 
-fn read_code(input: &Path, use_conditionals: bool) -> Vec<Mul> {
+fn read_code(input: &Path, use_conditionals: bool) -> Vec<usize> {
     let mut result = vec![];
     let data = std::fs::read_to_string(input).unwrap();
     if use_conditionals {
@@ -46,12 +41,12 @@ fn read_code(input: &Path, use_conditionals: bool) -> Vec<Mul> {
 
 fn part1(input: &Path) -> usize {
     let factors = read_code(input, false);
-    factors.iter().map(|f| f.a * f.b).sum()
+    factors.iter().sum()
 }
 
 fn part2(input: &Path) -> usize {
     let factors = read_code(input, true);
-    factors.iter().map(|f| f.a * f.b).sum()
+    factors.iter().sum()
 }
 
 pub fn main(input: &Path) -> (usize, usize) {
